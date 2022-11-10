eadfcc0f17a8d9e9e92868f01803b70fbcabe72f
pirisun <pyoram25@gamil.com>
2022-11-10 17:15:25 +0900

    클래스 삭제

--- lib/member.dart
+++ lib/member.dart
@@ -16,18 +16,6 @@ class _MemberPageState extends State<MemberPage> {
     );
   }
 }
-class ProfilePage extends StatefulWidget {
-  const ProfilePage({Key? key}) : super(key: key);
 
-  @override
-  State<ProfilePage> createState() => _ProfilePageState();
-}
-
-class _ProfilePageState extends State<ProfilePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
 
 
