7c9c9e19d59b8c0a637fc0c08abc36e12c68ffd5
HunSeokJeong <44901486+HunSeokJeong@users.noreply.github.com>
2022-11-21 14:53:37 +0900

    [feat] 서버 연결함수 거의 다 제작함.

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,3 +1,205 @@
+class Historylist {
+  List<Listhistory>? listhistory;
+
+  Historylist({this.listhistory});
+
+  Historylist.fromJson(Map<String, dynamic> json) {
+    if (json['listhistory'] != null) {
+      listhistory = <Listhistory>[];
+      json['listhistory'].forEach((v) {
+        listhistory!.add(new Listhistory.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.listhistory != null) {
+      data['listhistory'] = this.listhistory!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Listhistory {
+  int? id;
+  int? room;
+  Author? author;
+  String? createDate;
+  String? modifyDate;
+  int? event;
+  String? image;
+  String? text;
+
+  Listhistory(
+      {this.id,
+        this.room,
+        this.author,
+        this.createDate,
+        this.modifyDate,
+        this.event,
+        this.image,
+        this.text});
+
+  Listhistory.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    room = json['room'];
+    author =
+    json['author'] != null ? new Author.fromJson(json['author']) : null;
+    createDate = json['create_date'];
+    modifyDate = json['modify_date'];
+    event = json['event'];
+    image = json['image'];
+    text = json['text'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['room'] = this.room;
+    if (this.author != null) {
+      data['author'] = this.author!.toJson();
+    }
+    data['create_date'] = this.createDate;
+    data['modify_date'] = this.modifyDate;
+    data['event'] = this.event;
+    data['image'] = this.image;
+    data['text'] = this.text;
+    return data;
+  }
+}
+
+
+
+class InviteCheck {
+  List<Member>? memberList;
+
+  InviteCheck({this.memberList});
+
+  InviteCheck.fromJson(Map<String, dynamic> json) {
+    if (json['member_list'] != null) {
+      memberList = <Member>[];
+      json['member_list'].forEach((v) {
+        memberList!.add(new Member.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.memberList != null) {
+      data['member_list'] = this.memberList!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+
+class MyGroup {
+  List<GroupList>? groupList;
+
+  MyGroup({this.groupList});
+
+  MyGroup.fromJson(Map<String, dynamic> json) {
+    if (json['group_list'] != null) {
+      groupList = <GroupList>[];
+      json['group_list'].forEach((v) {
+        groupList!.add(new GroupList.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.groupList != null) {
+      data['group_list'] = this.groupList!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class GroupList {
+  int? id;
+  String? groupcode;
+  String? title;
+  List<Member>? member;
+  List<Member>? memberinvite;
+  int? author;
+
+  GroupList(
+      {this.id,
+        this.groupcode,
+        this.title,
+        this.member,
+        this.memberinvite,
+        this.author});
+
+  GroupList.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    groupcode = json['groupcode'];
+    title = json['title'];
+    if (json['member'] != null) {
+      member = <Member>[];
+      json['member'].forEach((v) {
+        member!.add(new Member.fromJson(v));
+      });
+    }
+    if (json['memberinvite'] != null) {
+      memberinvite = <Member>[];
+      json['memberinvite'].forEach((v) {
+        memberinvite!.add(new Member.fromJson(v));
+      });
+    }
+    author = json['author'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['groupcode'] = this.groupcode;
+    data['title'] = this.title;
+    if (this.member != null) {
+      data['member'] = this.member!.map((v) => v.toJson()).toList();
+    }
+    if (this.memberinvite != null) {
+      data['memberinvite'] = this.memberinvite!.map((v) => v.toJson()).toList();
+    }
+    data['author'] = this.author;
+    return data;
+  }
+}
+
+class Member {
+  String? nickname;
+  int? icon;
+  String? stateMessage;
+  User? user;
+
+  Member({this.nickname, this.icon, this.stateMessage, this.user});
+
+  Member.fromJson(Map<String, dynamic> json) {
+    nickname = json['nickname'];
+    icon = json['icon'];
+    stateMessage = json['state_message'];
+    user = json['user'] != null ? new User.fromJson(json['user']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['nickname'] = this.nickname;
+    data['icon'] = this.icon;
+    data['state_message'] = this.stateMessage;
+    if (this.user != null) {
+      data['user'] = this.user!.toJson();
+    }
+    return data;
+  }
+}
+
+
+
+
+
 class GETROOMLIST {
   List<Roomlist>? roomlist;
 
@@ -186,6 +388,7 @@ class Profile {
     return data;
   }
 }
+
 
 class User {
   int? id;
